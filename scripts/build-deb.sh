0a2be268f24ebbfe33e407618e97a0cf5c87e895
Uri Herrera <hello@uriherrera.com>
2024-03-12 04:16:32 -0600

    update dep

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -78,7 +78,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=probal31@gmail.com \
 	--provides=bonsai \
-	--requires="libc6,libgcc-s1,libgit2-1.1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 3.1.0+git\),mauikit-filebrowsing-git \(\>= 3.1.0+git\),mauikit-terminal-git \(\>= 3.1.0+git\),mauikit-texteditor-git \(\>= 3.1.0+git\)" \
+	--requires="libc6,libgcc-s1,libgit2-1.7,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 3.1.0+git\),mauikit-filebrowsing-git \(\>= 3.1.0+git\),mauikit-terminal-git \(\>= 3.1.0+git\),mauikit-texteditor-git \(\>= 3.1.0+git\)" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
