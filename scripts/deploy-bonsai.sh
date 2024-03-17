17f1bbe8c07dd0109437916052d3392a51c9f612
Uri Herrera <hello@uriherrera.com>
2024-03-16 18:59:32 -0600

    fix git cmd

--- scripts/deploy-bonsai.sh
+++ scripts/deploy-bonsai.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-BONSAI_VERSION=$([ -z $BRANCH_BONSAI ] && echo "3.1.0" || echo $BRANCH_BONSAI)
+BONSAI_VERSION=$([ -z $BRANCH_BONSAI ] && echo "master" || echo $BRANCH_BONSAI)
 
 echo "Cloning Bonsai from branch/tag $BONSAI_VERSION"
 
-git clone --depth 1 --branch https://invent.kde.org/maui/bonsai.git $BONSAI_VERSION
+git clone --depth 1 --branch $BONSAI_VERSION https://invent.kde.org/maui/bonsai.git
 
 cd bonsai
 cmake -DCMAKE_BUILD_TYPE="Release" \
--- scripts/deploy-mauikit-terminal.sh
+++ scripts/deploy-mauikit-terminal.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-MAUIKIT_TERMINAL_VERSION=$([ -z $BRANCH_MAUIKIT_TERMINAL ] && echo "master" || echo $BRANCH_MAUIKIT_TERMINAL)
+MAUIKIT_TERMINAL_VERSION=$([ -z $BRANCH_MAUIKIT_TERMINAL ] && echo "3.1.0" || echo $BRANCH_MAUIKIT_TERMINAL)
 
 echo "Cloning Mauikit Terminal from branch $MAUIKIT_TERMINAL_VERSION"
 
-git clone https://invent.kde.org/maui/mauikit-terminal.git --depth=1 -b $MAUIKIT_TERMINAL_VERSION
+git clone --depth 1 --branch  $MAUIKIT_TERMINAL_VERSION https://invent.kde.org/maui/mauikit-terminal.git
 
 cd mauikit-terminal
 mkdir -p build
