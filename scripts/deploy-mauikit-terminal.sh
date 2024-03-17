c8df11b91e419f751c126310d3d2e0afc3285671
Uri Herrera <hello@uriherrera.com>
2024-03-16 19:19:10 -0600

    fix tag

--- scripts/deploy-mauikit-terminal.sh
+++ scripts/deploy-mauikit-terminal.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-MAUIKIT_TERMINAL_VERSION=$([ -z $BRANCH_MAUIKIT_TERMINAL ] && echo "3.1" || echo $BRANCH_MAUIKIT_TERMINAL)
+MAUIKIT_TERMINAL_VERSION=$([ -z $BRANCH_MAUIKIT_TERMINAL ] && echo "v3.1" || echo $BRANCH_MAUIKIT_TERMINAL)
 
 echo "Cloning Mauikit Terminal from branch/tag $MAUIKIT_TERMINAL_VERSION"
 
-git clone --depth 1 --branch  $MAUIKIT_TERMINAL_VERSION https://invent.kde.org/maui/mauikit-terminal.git
+git clone --depth 1 --branch $MAUIKIT_TERMINAL_VERSION https://invent.kde.org/maui/mauikit-terminal.git
 
 cd mauikit-terminal
 mkdir -p build
